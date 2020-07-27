8aff28b012eb354275de3908aef52b402a974f98
Boogy83 <bozhidar.atanasov@abv.bg>
2020-07-27 13:50:06 +0300

    dd

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const config = require('./config');
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  console.log(config.dbURL)
+  console.log(config.dbURL, 'test')
   return mongoose.connect(config.dbURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
