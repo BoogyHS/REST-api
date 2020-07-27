4fc145291241acc50cc6dc1a9408a446d8bdc9de
Boogy83 <bozhidar.atanasov@abv.bg>
2020-07-27 13:22:13 +0300

    edit

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ const config = {
     },
     production: {
         port: process.env.PORT || 3000,
-        dbURL: "mongodb+srv://boogy:2j7XT6MjX6ARTYe@cluster0.airlw.mongodb.net/rest-api?retryWrites=true&w=majority",
+        dbURL: process.env.DB_URL_CREDENTIALS,
         origin: 'http://localhost:3000'
     }
 };
